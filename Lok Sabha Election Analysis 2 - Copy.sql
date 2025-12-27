f05327139e3be74619aa274cd8c06466d8967b06
rahul-120805 <rahulmahadikgrt@gmail.com>
2025-12-27 14:32:49 +0530

    Add files via upload
    Lok Sabha Analysis Part 2

---
+++ Lok Sabha Election Analysis 2 - Copy.sql
@@ -0,0 +1,7 @@
+SELECT * FROM constituencywise_details;
+SELECT * FROM constituencywise_results;
+SELECT * FROM partywise_results;
+SELECT * FROM states;
+SELECT * FROM statewise_results;
+
+USe [Lok Sabha Election Database]; 
